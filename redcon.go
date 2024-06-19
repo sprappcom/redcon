edb825dccb8edd9291909b4d2bfe88e2db92d638
sprappcom <156875069+sprappcom@users.noreply.github.com>
2024-06-19 04:15:03 -0400

    Update redcon.go

--- redcon.go
+++ redcon.go
@@ -51,6 +51,14 @@ type Conn struct {
 }
 
 
+// Command represent a command
+type Command struct {
+	// Raw is a encoded RESP message.
+	Raw []byte
+	// Args is a series of arguments that make up the command.
+	Args [][]byte
+}
+
 func parseInt(b []byte) (int, bool) {
 	if len(b) == 1 && b[0] >= '0' && b[0] <= '9' {
 		return int(b[0] - '0'), true
